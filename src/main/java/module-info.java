6a88845a4a0ef957ebb0f36b4764cd0b40a5fc7d
dbc2201 <divyanshb22@gmail.com>
2022-02-18 21:50:40 +0530

    added library for log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.dbc.github.sninventory {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
 
     opens io.dbc.github.sninventory to javafx.fxml;
